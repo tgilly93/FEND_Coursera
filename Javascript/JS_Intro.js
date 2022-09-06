fbeea5590a89ec9cc949d208ca949352d6bfbbca
Terry Gilmore <tgilly93@gmail.com>
2022-09-06 17:24:51 -0400

    Folder maintenance for HtML. Intro to JS.

---
+++ Javascript/JS_Intro.js
@@ -0,0 +1,79 @@
+console.log('Hello World')
+// this is a comment!
+/* this is a multi-line comment on a single line */
+console.log("%cHello, World", "color: blue; font-size: 40px")
+// Changing appearance of text.
+var greeting = "Hello World"
+console.log(greeting)
+// Assigning a value to a variable.
+var petDog = ("Rex")
+var petCat = ("Pepper")
+console.log(petDog)
+console.log(petCat)
+console.log("My pet dog's name is:", petDog)
+console.log("My pet cat's name is:", petCat)
+var catSound = ("purr")
+var dogSound = ("woof")
+console.log(petDog, "says", dogSound)
+console.log(petCat, "says", catSound)
+catSound = "meow"
+console.log(petCat, "now says", catSound)
+// more practice.
+
+var currentTime = 10;
+console.log(currentTime > 9 && currentTime < 17);
+var currentTime = 7;
+console.log(currentTime < 9 || currentTime > 17);
+var petHungry = true
+console.log('Feeding the pet')
+console.log("Pet is hungry: ", !petHungry)
+console.log(petHungry);
+console.log(22 % 5); // 2
+console.log(5 == 5);
+console.log(5 == "5");
+console.log(5 != "5");
+console.log(5 !== "5");
+console.log("inter" + "net"); // "internet"
+console.log("note" + "book"); // "notebook"
+console.log(365 + " days"); // "365 days"
+console.log(12 + " months"); // "12 months"
+console.log(1 + "2");
+var overtime = 1;
+overtime += 2;
+overtime += 1;
+overtime += 2;
+overtime += 3;
+console.log(overtime); // 9
+var longString = "";
+longString += "Once";
+longString += " upon";
+longString += " a";
+longString += " time";
+longString += "...";
+console.log(longString); // "Once upon a time..."
+var score = 8
+console.log("mid-level skills:", score > 0 && score < 10)
+var timeRemaining = 0
+var energy = 10
+console.log("Game over: ", timeRemaining == 0 || energy == 0)
+var num1 = 2
+var num2 = 5
+var test1 = num1 % 2
+var test2 = num2 % 2
+var result1 = test1 == 0
+var result2 = test2 == 0 
+console.log("Is", num1, "an even number?", result1)
+console.log("Is", num2, "an even number?", result2)
+var num1 = 2
+var num2 = 5
+var test1 = num1 % 2
+var test2 = num2 % 2
+var result1 = test1 == 0
+var result2 = test2 == 0 
+console.log("Is", num1, "an even number?", result1)
+console.log("Is", num2, "an even number?", result2)
+var counter = 0
+counter += 5
+counter += 3
+console.log(counter)
+// Practice with all types of operators.
