0f13bf39220fcb8fa88387bcc074b350f3ebcd44
Terry Gilmore <tgilly93@gmail.com>
2022-09-10 13:18:19 -0400

    Final project code/Testing with Jest.

--- addFive.test.js
+++ addFive.test.js
@@ -2,5 +2,5 @@ const { default: TestRunner } = require("jest-runner");
 const addFive = require(`./addFive`);
 
 test('returns the number plus 5', () => {
-    expect(addFive(1)).toBe(6);
+    expect(addFive(1)).toBe(6);cd
 })
